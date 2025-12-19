c0823c1c7fa428a0556082dbce56aafee8ae8ca5
AnnieKMoon <mkw005@naver.com>
2025-12-19 17:07:25 +0900

    251219 프론트엔드 세팅

---
+++ front-end/src/components/pages/DeptPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import DeptList from '../dept/DeptList'
+import Header from '../include/Header'
+import Footer from '../include/Footer'
+
+const DeptPage = () => {
+  return (
+    <>
+      <Header />
+      <h2>부서관리</h2>
+      <DeptList />
+      <Footer />
+    </>
+  )
+}
+
+export default DeptPage
---
+++ front-end/src/components/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Header from '../include/Header'
+import Footer from '../include/Footer'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, libero?
+      Eaque deleniti omnis cumque! In tempora quibusdam eveniet aspernatur enim?
+      Blanditiis deleniti explicabo itaque quam nihil est ipsa odit quis.
+      Delectus doloribus eaque incidunt ea magni rerum est fugiat voluptatibus.
+      Molestias, at voluptate facere impedit necessitatibus consectetur exercitationem eius veniam?
+      Hic sunt iure eveniet itaque molestias adipisci porro error quis!
+      Ducimus voluptas molestias, provident possimus voluptate quam qui mollitia porro!
+      Vero animi provident tempore ipsam, praesentium ea? Totam, voluptatibus quia.
+      Facere, impedit atque. Cupiditate voluptatum totam, consectetur hic aliquid voluptates.
+      Ipsam, rerum veritatis architecto vero eveniet hic assumenda saepe eaque!</p>
+      <Footer />
+
+    </div>
+  )
+}
+
+export default HomePage
